0aaef5d107d70705322ba46fbd70bdb063ea3df4
Andy Tran <andy.tran11996@gmail.com>
2019-04-27 16:21:12 +0200

    change homepage

--- localman.rb
+++ localman.rb
@@ -1,7 +1,7 @@
 class Localman < Formula
   desc "A mini script that helps you manage your local domains easier"
-  homepage ""
-  url "https://github.com/Andycillin/localman/releases/download/v1.0.0/localman-1.0.0.tar.gz"
+  homepage "https://github.com/Andycillin/localman/"
+  url "https://github.com/andycillin/localman/releases/download/v1.0.0/localman-1.0.0.tar.gz"
   sha256 "59a1f696807d367a3d19b8493136db565f90f62d6be7fcc8643d1af062b982ef"
 
   def install
