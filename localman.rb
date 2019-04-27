e253d6c79c1d148a1ad14ae3758fa8d9c7904ccb
Andy Tran <andy.tran11996@gmail.com>
2019-04-27 16:38:35 +0200

    change checksum

--- localman.rb
+++ localman.rb
@@ -2,7 +2,7 @@ class Localman < Formula
   desc "A mini script that helps you manage your local domains easier"
   homepage "https://github.com/Andycillin/localman/"
   url "https://github.com/andycillin/localman/releases/download/v1.0.0/localman-1.0.0.tar.gz"
-  sha256 "59a1f696807d367a3d19b8493136db565f90f62d6be7fcc8643d1af062b982ef"
+  sha256 "d70df03bff238a4151e1c4caec457f45b1542e10949f1d77f0b4a455f420c94f"
 
   def install
     bin.install "localman"
