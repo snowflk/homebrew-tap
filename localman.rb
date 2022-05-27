d6e819860e355d6d1110f267fc9e6a783d4c4f4a
Phuc Tran <andy.tran11996@gmail.com>
2022-05-27 19:08:39 +0200

    Update localman.rb

--- localman.rb
+++ localman.rb
@@ -1,7 +1,7 @@
 class Localman < Formula
   desc "A mini script that helps you manage your local domains easier"
-  homepage "https://github.com/Andycillin/localman/"
-  url "https://github.com/andycillin/localman/releases/download/v1.0.0/localman-1.0.0.tar.gz"
+  homepage "https://github.com/snowflk/localman/"
+  url "https://github.com/snowflk/localman/releases/download/v1.0.0/localman-1.0.0.tar.gz"
   sha256 "d70df03bff238a4151e1c4caec457f45b1542e10949f1d77f0b4a455f420c94f"
 
   def install
