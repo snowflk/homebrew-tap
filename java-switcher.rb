5ed5af55bc43c6fd369001f0de1faf0ead457157
Phuc Tran <andy.tran11996@gmail.com>
2022-05-27 19:09:07 +0200

    Update java-switcher.rb

--- java-switcher.rb
+++ java-switcher.rb
@@ -3,8 +3,8 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class JavaSwitcher < Formula
   desc "A small script that helps you switch Java version easier"
-  homepage "https://github.com/Andycillin/java-switcher"
-  url "https://github.com/Andycillin/java-switcher/releases/download/v1.0/java-switcher.zip"
+  homepage "https://github.com/snowflk/java-switcher"
+  url "https://github.com/snowflk/java-switcher/releases/download/v1.0/java-switcher.zip"
   sha256 "61ec224bf9b606993d71cdec3f1de57dc6ecd05ccf02b4d576518f9376cdcfe2"
   # depends_on "cmake" => :build
 
