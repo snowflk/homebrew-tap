db41342a24c1c43fd2eb5e35eb655e8adcbb683e
Phuc Tran <tptran.sw@gmail.com>
2026-03-05 12:30:55 +0100

    Update localman.rb

--- localman.rb
+++ localman.rb
@@ -1,20 +1,11 @@
 class Localman < Formula
   desc "A mini script that helps you manage your local domains easier"
   homepage "https://github.com/snowflk/localman"
-  url "https://github.com/snowflk/localman/releases/download/v1.2.0/localman-1.2.0.tar.gz"
-  sha256 "4a345f6863a86c1fc66597dc1a37b251cedeab27cd959467f42126ae9808e89a"
+  url "https://github.com/snowflk/localman/releases/download/v1.2.1/localman-1.2.1.tar.gz"
+  sha256 "1c3d388a20876fba04bcd54a8e696476619490d69e162e4891d57e34d436052e"
   license "MIT"
-
-  livecheck do
-    url :stable
-  end
 
   def install
     bin.install "localman"
   end
-
-  test do
-    assert_predicate bin/"localman", :executable?
-  end
-
 end
